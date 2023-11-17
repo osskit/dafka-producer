9592bcd2bf5951f3f53dc87de7f5ace2587b03da
Guy Segal <guysegal.il@gmail.com>
2023-11-17 16:27:03 +0200

    .

--- tests/specs/useDefaultPartitioner.spec.ts
+++ tests/specs/useDefaultPartitioner.spec.ts
@@ -2,7 +2,7 @@ import delay from 'delay';
 
 import type {Orchestrator} from '../testcontainers/orchestrator.js';
 import {start} from '../testcontainers/orchestrator.js';
-import { sortBy } from 'lodash-es';
+import {sortBy} from 'lodash-es';
 
 const topic = 'my-topic';
 
@@ -14,7 +14,7 @@ describe('tests', () => {
             {
                 KAFKA_BROKER: 'kafka:9092',
                 MAX_BLOCK_MS: '1000',
-                USE_GROUP_PARTITIONER: 'false'
+                USE_GROUP_PARTITIONER: 'false',
             },
             [topic]
         );
@@ -28,19 +28,23 @@ describe('tests', () => {
     });
 
     it('produce with custom group partitioning', async () => {
-        await Promise.all([1,2,3,4,5].map(i => orchestrator.dafkaProducer.produce([
-            {
-                topic,
-                key: `dd387004-202e-487d-9c2d-1f5d6fea914f_someKey${i}`,
-                value: {data: 'foo'},
-            },
-        ])))
+        await Promise.all(
+            [1, 2, 3, 4, 5].map((i) =>
+                orchestrator.dafkaProducer.produce([
+                    {
+                        topic,
+                        key: `dd387004-202e-487d-9c2d-1f5d6fea914f_someKey${i}`,
+                        value: {data: 'foo'},
+                    },
+                ])
+            )
+        );
 
         await delay(5000);
-        
-        const admin = orchestrator.kafkaClient.admin()
-        const metadata = await admin.fetchTopicOffsets(topic)
-        const partitions = metadata.filter(x => parseInt(x.offset) > 0).map(x => x.partition)
-        expect(sortBy(partitions)).toEqual([15, 32, 46, 60, 93])
+
+        const admin = orchestrator.kafkaClient.admin();
+        const metadata = await admin.fetchTopicOffsets(topic);
+        const partitions = metadata.filter((x) => parseInt(x.offset) > 0).map((x) => x.partition);
+        expect(sortBy(partitions)).toEqual([15, 32, 46, 60, 93]);
     });
 });
